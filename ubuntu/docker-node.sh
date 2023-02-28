9c828eda700aeb122711278e0aef1dfc329f33d7
Cryptospider !! <cryptospider671@gmail.com>
2023-02-28 19:54:25 +0000

    Add nvm to path

--- ubuntu/docker-node.sh
+++ ubuntu/docker-node.sh
@@ -49,8 +49,13 @@ $SUDO usermod -aG docker $USER
 # Instal node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-# Source .bashrc or .zshrc file
+# Source .bashrc
 source ~/.bashrc
+
+# Add nvm to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Node version
 read -p "Enter node version to install: " NODE_VERSION
